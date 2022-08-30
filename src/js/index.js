c32e5cdfda93e3f5ee8d3e941c6e70828b09b1d4
Symetra <105522993+SymetraDev7@users.noreply.github.com>
2022-08-30 15:08:34 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,6 @@ buttonToggleMode.addEventListener("click", () => {
   let page = document.querySelector("body");
   page.classList.toggle("dark");
 
-  page.classList[0] === "dark"
-    ? (buttonToggleMode.style.backgroundImage =
-        "url('../src/images/SunFull.png')")
-    : (buttonToggleMode.style.backgroundImage = "url('../src/images/Sun.png')");
-});
 
 let buttonToggleMenu = document.querySelector(".icon-menu");
 
